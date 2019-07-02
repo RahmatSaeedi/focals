bc32d7c8a413f8f3dd9ff382d73587fe38f5f35c
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 23:42:08 -0600

    animates a spinner on the console, (character |/-\|/-\| in that order), as though it is spinning

--- spinner.js
+++ spinner.js
@@ -5,11 +5,11 @@ const sentence = "|/-\\|/-\\|";
 let i = 0;
 
 
-const spinner1 = () => setTimeout(() => {
+const spinner = () => setTimeout(() => {
   process.stdout.write(`\r${sentence[i++]}    `);
-  if (i < sentence.length) spinner1();
+  if (i < sentence.length) spinner();
   if (i === sentence.length) process.stdout.write('\n');
 }, 200);
 
 
-spinner1();
+spinner();
