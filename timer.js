6215f7162311f41b39a2783673463369dbf37b5d
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-02 10:39:24 -0600

    `beeps` after a specified duration. Inputs should be non-negative

--- timer.js
+++ timer.js
@@ -4,7 +4,7 @@
 // jshint esversion : 6
 
 process.argv.slice(2).forEach(e => {
-  if (! isNaN(e)) {
+  if (!isNaN(e) && e >= 0) {
     setTimeout(() => {
       process.stdout.write(`\x07`);
     }, e * 1000);
