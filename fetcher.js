72c32bb7699ce5da1f4a3e5497b535fba38f4c97
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-05 12:14:51 -0600

    A small command line node app which takes a URL and a local file path, as command-line arguments, and downloads the resource to the specified path.

--- fetcher.js
+++ fetcher.js
@@ -11,13 +11,18 @@ if (process.argv.length === 4) {
   
   request(requestedResource, (err, status, body) => {
     if (!err) {
-      fs.writeFile(localFilePath, body, (err) => {
-        if (!err) {
-          console.log('Fetched successfully.');
-        } else {
-          console.log("Could not save to the the specified path.", err);
-        }
-      });
+      if (body && status.statusCode === 200) {
+        fs.writeFile(localFilePath, body, (err) => {
+          if (!err) {
+            console.log('Fetched successfully.');
+          } else {
+            console.log("Could not save to the the specified path.", err);
+          }
+        });
+      } else {
+        console.log("Body of the requested URL is empty or the statusCode recived was not 200");
+        console.log(`Status Code: ${status.statusCode} \r\n Staus: \r\n ${status} \r\n Body: \r\n ${body}`);
+      }
     } else {
       console.log("Could not fetch the specified webpage.", err);
     }
