5868c0f691318d94b1ef4865513ea9340c6256b7
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-07 19:22:11 -0600

    OOp & Classes

---
+++ lightCoin.js
@@ -0,0 +1,49 @@
+// jshint esversion : 6
+
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() { // can waithdraw without limit
+    this.account.balance -= this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  commit() {
+    this.account.balance -= this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
+
+
+
+
+// DRIVER CODE BELOW
+// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
+console.log('Balance:', myAccount.balance);
+
+t1 = new Withdrawal(50.25, myAccount);
+t1.commit();
+
+console.log('Balance:', myAccount.balance);
+
